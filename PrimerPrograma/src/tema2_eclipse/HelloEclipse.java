c56faa529c1d2a70c561a42da1d1e5fd01ccb476
smefrag2310 <smefrag2310@g.educaand.es>
2025-10-03 20:17:44 +0200

    Add files via upload

---
+++ PrimerPrograma/src/tema2_eclipse/HelloEclipse.java
@@ -0,0 +1,35 @@
+package tema2_eclipse;
+
+public class HelloEclipse {
+
+	public static void main(String[] args) {
+		 // Comentario de una línea: primer programa
+        System.out.println("¡Mi primer programa!");
+
+        System.out.println("Reglas básicas de Java:");
+        System.out.println("1) Distingue MAYÚSCULAS y minúsculas");
+        System.out.println("2) Cada instrucción termina con ;");
+
+        /* Comentario de varias líneas:
+           Autor: Santiago Mefle Ragel
+           Fecha: 03/10/2025
+        */
+
+        int contadorMensajes = 0;
+        contadorMensajes++;
+        System.out.println("Mensajes impresos: " + contadorMensajes);
+        
+        int x = 3;
+        System.out.print("Este código no está");
+        System.out.print(" muy bien ");
+        System.out.print("tabulado");
+        x = 7;
+        System.out.println();
+
+       
+    }
+
+
+	}
+
+
